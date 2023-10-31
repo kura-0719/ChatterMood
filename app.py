8a82127c76befd9281ce3bc8f216ff505fe525a7
takumi <takura.010719@gmail.com>
2023-10-31 16:44:37 +0900

    パターンを増やした、portの設定

--- app.py
+++ app.py
@@ -339,4 +339,4 @@ def exclamation_response(user_message, patterns):
 if __name__ == '__main__':
     with app.app_context():
         db.create_all()
-    app.run(debug=True)
+    app.run(debug=True,port=5555)
